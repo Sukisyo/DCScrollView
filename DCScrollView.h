84dad773410cb01f2da50e2413dc864a62aa0a64
Sukisyo <1196378539@qq.com>
2016-03-01 21:44:08 +0800

    对原有的控件进行完善，添加了加载网络图片的方法

--- DCScrollView.h
+++ DCScrollView.h
@@ -8,13 +8,29 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ *  因为实际app上滚动轮播器的图片都是有点击事件的，所有在这里设置该自定义控件
+ *  的代理方法来实现这个功能
+ */
+@protocol PostForInfoDelegate <NSObject>
+
+- (void)postForInfoWithImageid:(NSString *)imageid;
+
+@end
+
 @interface DCScrollView : UIView
 
+@property (nonatomic,weak) id<PostForInfoDelegate> delegate;
 
 //自定义初始化方法，指定scrollView大小和图片轮播的对象
-- (instancetype)initWithFrame:(CGRect)frame andImages:(NSArray*)images;
-
-
+/**
+ *  此方法传入的数组是网络加载的图片url的数组
+ */
+- (instancetype)initWithFrame:(CGRect)frame andImageUrls:(NSArray *)urls;
+/**
+ *  此方法传入的数组是本地的image图片数组
+ */
+- (instancetype)initWithFrame:(CGRect)frame andImages:(NSArray *)images;
 
 
 @end
